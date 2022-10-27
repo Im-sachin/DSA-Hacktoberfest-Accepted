082ce17ae92a39300ee91ca563da44eb3a1cbc5c
Shyam0169 <116086882+Shyam0169@users.noreply.github.com>
2022-10-27 13:21:54 +0530

    Create Kadane_algorithm.cpp

---
+++ C++/Array/Kadane_algorithm.cpp
@@ -0,0 +1,24 @@
+lass Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int csum = arr[0];
+        int osum = arr[0];
+        for(int i=1; i<n; i++){
+            if(csum >= 0){
+                csum += arr[i];
+            }else{
+                csum = arr[i];
+            }
+            if(csum > osum){
+                osum = csum;
+            }
+        }
+        return osum;
+        
+    }
+};
